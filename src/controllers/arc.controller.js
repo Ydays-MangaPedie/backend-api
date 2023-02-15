e9b8334ee22ae3a5d5b9b5bd797f8558a0b16b2b
okabasele <ocekabasele@gmail.com>
2023-02-15 16:34:58 +0100

    refacto arc

--- src/controllers/arc.controller.js
+++ src/controllers/arc.controller.js
@@ -2,7 +2,8 @@ const { getMultiple } = require("../services/database");
 
 exports.getArcsByMangaID = async (req, res, next) => {
   try {
-    res.json(await getMultiple("arc",`WHERE idmanga = ${req.query.idmanga}`, req.query.page));
+    const result = await getMultiple("arc",`WHERE idmanga = ${req.query.idmanga}`, req.query.page);
+    res.json(result);
   } catch (err) {
     next(new Error(`Error while getting arcs \n ${err.message}`));
   }
